0c68440ca2041ddba669e6331ac151c089b1f55c
Xavier Hreben <projectfigirl@gmail.com>
2023-10-05 13:49:18 -0400

    Added code to allow image content to be sent to Azure Analysis account, and the response to be parsed.

--- project1_webapp/Pages/Upload.cshtml.cs
+++ project1_webapp/Pages/Upload.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.ComponentModel;
 
 namespace project1_webapp.Pages {
 	public class UploadModel : PageModel {
@@ -13,7 +15,14 @@ namespace project1_webapp.Pages {
 			_logger = logger;
 		}
 
+		// no result yet, let them upload
 		public void OnGet() {
+			//FileUploaded = JsonSerializer.Deserialize<IFormFile>(TempData["UploadedFile"] as string);
+
+			if (TempData["UploadedFile"] != null) {
+				_logger.LogInformation("passed success");
+				Console.WriteLine(TempData["UploadedFile"]);
+			}
 		}
 
 		public async Task<IActionResult> OnPostAsync() {
@@ -21,24 +30,70 @@ namespace project1_webapp.Pages {
 			if (UploadFile != null && UploadFile.Length > 0) {
 
 				// Specify the folder where you want to store uploaded files
-				var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+				//var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
 
 				// Create the folder if it doesn't exist
-				Directory.CreateDirectory(uploadsFolder);
+				//Directory.CreateDirectory(uploadsFolder);
 
 				// Generate a unique file name (you can customize this)
-				var uniqueFileName = Guid.NewGuid().ToString() + "_" + UploadFile.FileName;
+				//var uniqueFileName = Guid.NewGuid().ToString() + "_" + UploadFile.FileName;
 
 				// Combine the folder path and file name
-				var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+				//var filePath = Path.Combine(uploadsFolder, uniqueFileName);
 
 				// Save the uploaded file
-				using (var stream = new FileStream(filePath, FileMode.Create)) {
-					await UploadFile.CopyToAsync(stream);
+				//using (var stream = new FileStream(filePath, FileMode.Create)) {
+				//	await UploadFile.CopyToAsync(stream);
+				//}
+
+				// send file for processing
+				// retrieve and upload page, passing returned json
+				//TempData["UploadedFile"] = "Testing Transfer";
+
+				using (var httpClient = new HttpClient()) {
+
+					var queryParams = new Dictionary<string, string> { 
+						{ "visualFeatures", "Description,Tags,Objects" } // Replace with the desired features.
+					};
+
+					var uriBuilder = new UriBuilder(@"https://multiaiservices-xh1.cognitiveservices.azure.com/vision/v3.1/analyze");
+					uriBuilder.Query = string.Join("&", queryParams.Select(x => $"{x.Key}={Uri.EscapeDataString(x.Value)}"));
+					//var requestString = "{\"authCode\": \"0000000001Nk1EEhZ3pZ73z700271891\" }";
+					httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "79788f0317cd43c7931dbd5affb63db6");
+					// Setup the HttpClient and make the call and get the relevant data.
+					//httpClient.BaseAddress = new Uri("https://multiaiservices-xh1.cognitiveservices.azure.com");
+					//string theUrl = @"https://multiaiservices-xh1.cognitiveservices.azure.com/vision/v3.1/analyze";
+					//var request = new HttpRequestMessage(HttpMethod.Post, $"/vision/v3.1/analyze");
+					var content = new MultipartFormDataContent();
+					content.Add(new StreamContent(UploadFile.OpenReadStream()), "Image");
+					//request.Content = content;
+					
+					var response = await httpClient.PostAsync(uriBuilder.Uri, content);
+					var responseContent = await response.Content.ReadAsStringAsync();
+					dynamic theObj = JsonConvert.DeserializeObject<dynamic>(responseContent);
+					Console.WriteLine(JsonConvert.SerializeObject(response.Headers.ToList()));
+					Console.WriteLine(theObj);
+
+					foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(theObj)) {
+						Console.WriteLine("PROP: " + prop.Name);
+					}
+					Console.WriteLine(theObj.description["captions"][0]["text"]);
+
+					if (response.IsSuccessStatusCode) {
+						Console.WriteLine("Successful");
+						Console.WriteLine(responseContent);
+					} else {
+						Console.WriteLine("Not successful");
+					}
+
+					// TODO
+					// now place data in object
+					// pass data to next page for display
 				}
 
 				// Redirect to a success page or display a success message
-				return Page();//RedirectToPage("UploadSuccess");
+				//return RedirectToPage("/Upload", new { FileUploaded = true });
+				return Page();
 			}
 
 			// Handle invalid file uploads
